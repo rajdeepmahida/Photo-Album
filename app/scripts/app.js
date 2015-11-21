a88c0b2304bda3099fae71287f860d901fdd42bf
Rajdeep Mahida <rajdeepmahida@gmail.com>
2015-11-21 13:13:08 +0000

    Changes made by Rajdeep Mahida (rajdeepmahida@gmail.com). Step 1 to 7 of photo ablum app complete.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,18 @@
 (function () {
+ 
     document.addEventListener("deviceready", function () {
+        var everlive = new Everlive("bkbIik1VKAsqGWxw");
         window.listview = kendo.observable({
             addImage: function () {
-                var success = function (data) {
-                    $("#images")
-                        .data("kendoMobileListView")
-                        .prepend(["data:image/jpeg;base64," + data]);
+                  var success = function (data) {
+                    everlive.Files.create({
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                        ContentType: "image/jpeg",
+                        base64: data
+                    }).then(loadPhotos);
                 };
+
+
                 var error = function () {
                     navigator.notification.alert("Unfortunately we could not add the image");
                 };
@@ -22,10 +28,23 @@
             skin: "nova"
         });
 
-        $("#images").kendoMobileListView({
-            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
-            template: "<img src='#: data #'>"
-        });
+      function loadPhotos() {
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                for (var i = data.result.length - 1; i >= 0; i--) {
+                    var image = data.result[i];
+                    files.push(image.Uri);
+                }
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        }
+        loadPhotos();
+
         navigator.splashscreen.hide();
     });
+
+
 }());
